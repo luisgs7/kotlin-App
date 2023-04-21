431dd0486994c03346f28eae0cb995e533c0c6f2
luisgs7 <luisgsilva270@gmail.com>
2023-04-21 11:35:06 -0300

    refactor code

--- app/src/main/java/com/iadevlab/appkt/MainActivity.kt
+++ app/src/main/java/com/iadevlab/appkt/MainActivity.kt
@@ -2,22 +2,24 @@ package com.iadevlab.appkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btn: Button
+    private lateinit var textApp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btn = findViewById<Button>(R.id.btn_id)
-        var text_app = findViewById<TextView>(R.id.text_id)
+        btn = findViewById(R.id.btn_id)
+        textApp = findViewById(R.id.text_id)
 
-        btn.setOnClickListener({ view ->
-            text_app.setText("Araguaína")
-        })
+        btn.setOnClickListener {
+            textApp.text = getString(R.string.city)
+        }
     }
 
 }
